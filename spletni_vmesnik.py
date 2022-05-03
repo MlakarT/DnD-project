bb77d9b227d99d04d6aa9b99830cc4ca14e69167
Mlakar na faksu <timo.mlakar@gmail.com>
2022-05-03 16:28:39 +0200

    Imported files, created a map

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,11 +1,23 @@
 #first we play a little with the code
 import bottle
+from numpy import matrix
 import kocke
+import linear_congruetal_generator as lcg
+from map_generator import Map
+from map_generator import generate
+
+displayed_map = Map(generate())
+displayed_map.exception_library()
+displayed_map.grid_matrix()
+
 
 @bottle.get('/')
 def osnovni_zaslon():
-    return bottle.template('spletna_stran.tpl',
-    standardni_set_kock = kocke.standard_set)
+    return bottle.template('U:\DnD-project\spletna_stran.tpl',
+    standardni_set_kock = kocke.standard_set,
+    current_map = Map((generate())),
+    seed = displayed_map.seed,
+    grid = displayed_map.matrix)
     # return """
     # <h1>Opravila</h1>
     # <h2>nekaj opravil</h2>
