092656d99f4af0eb7e9b09af837683732f9264ff
MlakarT <timo.mlakar@gmail.com>
2022-05-14 12:47:28 +0200

    Added incredibly barebones display method

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -5,21 +5,25 @@ from bottle import error
 import model
 from model import Map
 
-displayed_map = Map(model.generate())
+displayed_map = Map('1208112345')
 displayed_map.exception_library()
 displayed_map.grid_matrix()
+width, heigth = displayed_map.x, displayed_map.y
 
-@bottle.route('/static/spletna_stran_css.css')
-def server_static(datoteka):
-    return static_file(datoteka, root='spletna_stran_css.css')
+
 
 @bottle.get('/')
 def osnovni_zaslon():
     return bottle.template('spletna_stran.html',
-    standardni_set_kock = model.STANDARDNI_SET,
+    standardni_set_kock = model.STANDARD_SET,
     current_map = displayed_map,
     seed = displayed_map.seed,
+    x = width, y = heigth,
     grid = displayed_map.matrix)
+
+# @bottle.route('/static/spletna_stran_css.css')
+# def server_static(datoteka):
+#     return static_file(datoteka, root='spletna_stran_css.css')
 
 @bottle.error(404)
 def error404(error):
