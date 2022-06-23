8b1019541c49021d6ae820de14aff03c423cd1b7
MlakarT <timo.mlakar@gmail.com>
2022-06-23 15:47:16 +0200

    fixed login/json writing

--- model.py
+++ model.py
@@ -81,7 +81,7 @@ class Control:
     @classmethod
     def from_dict(cls, slovar):
         return cls(
-            users=[User.from_dict(d) for d in slovar["users"]]
+            users=[User.from_dict(d) for d in slovar.keys()]
         )
 
     def to_file(self, filename):
@@ -93,5 +93,74 @@ class Control:
         with open(filename) as file:
             return cls.from_dict(json.load(file))
 
-test_user = User("mlakar", "pass", "mlakar", 1208112345, [],[1,1,1,1])
-test_control = Control(users=[test_user])
+test = Control(
+        users=[
+            User(
+                "mlakar",
+                "pass",
+                "mlakar", 
+                1208112345, 
+                [],
+                [1,1,1,1]
+            )
+        ]
+    )
+
+# @dataclass
+# class Uporabnik:
+#     uporabnisko_ime: str
+#     zasifrirano_geslo: str
+
+#     @staticmethod
+#     def zasifriraj_geslo(geslo_v_cistopisu):
+#         return "XXX" + geslo_v_cistopisu[::-1] + "XXX"
+
+#     def ima_geslo(self, geslo_v_cistopisu):
+#         return self.zasifriraj_geslo(geslo_v_cistopisu) == self.zasifrirano_geslo
+    
+#     def nastavi_novo_geslo(self, geslo_v_cistopisu):
+#         self.zasifrirano_geslo = self.zasifriraj_geslo(geslo_v_cistopisu)
+
+#     def v_slovar(self):
+#         return {
+#             "uporabnisko_ime": self.uporabnisko_ime,
+#             "zasifrirano_geslo": self.zasifrirano_geslo,
+#         }
+
+#     @classmethod
+#     def iz_slovarja(cls, slovar):
+#         return cls(
+#             uporabnisko_ime=slovar["uporabnisko_ime"],
+#             zasifrirano_geslo=slovar["zasifrirano_geslo"],
+#         )
+
+
+# @dataclass
+# class VseSkupaj:
+#     uporabniki: List[Uporabnik]
+
+#     def poisci_uporabnika(self, uporabnisko_ime, geslo_v_cistopisu=None):
+#         for uporabnik in self.uporabniki:
+#             if uporabnik.uporabnisko_ime == uporabnisko_ime:
+#                 if geslo_v_cistopisu is None or uporabnik.ima_geslo(geslo_v_cistopisu):
+#                     return uporabnik
+
+#     def v_slovar(self):
+#         return {
+#             "uporabniki": [uporabnik.v_slovar() for uporabnik in self.uporabniki],
+#         }
+
+#     @classmethod
+#     def iz_slovarja(cls, slovar):
+#         return cls(
+#             uporabniki=[Uporabnik.iz_slovarja(sl) for sl in slovar.keys()]
+#         )
+
+#     def v_datoteko(self, ime_datoteke):
+#         with open(ime_datoteke, "w") as f:
+#             json.dump(self.v_slovar(), f, ensure_ascii=False, indent=4)
+
+#     @classmethod
+#     def iz_datoteke(cls, ime_datoteke):
+#         with open(ime_datoteke) as f:
+#             return cls.iz_slovarja(json.load(f))
--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,36 +1,65 @@
 #first we play a little with the code
 import bottle
 import model
-from model import generate
-from model import Map
+from model import Control
+#from model import VseSkupaj
+
+with open('secret.txt') as tf:
+    SECRET = tf.read()
+vse_skupaj = Control.from_file('state.json')
+#vse_skupaj = VseSkupaj.iz_datoteke('state.json')
+
+def save_all():
+    vse_skupaj.to_file('state.json')
+
+@bottle.post('/user_login/')
+def login():
+    username = bottle.request.forms.getunicode("username")
+    password = bottle.request.forms.getunicode("pass")
+    user = vse_skupaj.find_user(username, password)
+    if user:
+        bottle.response.set_cookie("username", username, path='/', secret=SECRET)
+        bottle.redirect('/')
+    else:
+        return "Incorrect username/password! Please try again."
+
 
 def url_mape(seed:int):
     """Turn seed into map url of the form '/map/xxxxxxxxxx'"""
     return f"/map/{seed}"
 
-@bottle.get('/static/<ime_dat:path>')
-def server_static(ime_dat):
-  pot = 'static/'
-  return bottle.static_file(ime_dat, root=pot)
+def save_state():
+    Control.to_file("state.json")
 
 @bottle.get('/')
 def osnovni_zaslon():
     bottle.redirect('/welcome_page/')
 
+@bottle.get('/static/<ime_dat:path>')
+def server_static(ime_dat):
+  pot = 'static/'
+  return bottle.static_file(ime_dat, root=pot)
+  
 @bottle.get('/welcome_page/')
 def display_welcome():
-    return bottle.template('views\welcome.html')
+    return bottle.template('views/welcome.html',
+    )
 
 @bottle.get('/map/<map_seed:int>')
 def display_map(map_seed:int):
     if map_seed == 0:
         grid = [[]]
-        return bottle.template('views/stran_mapa', grid = grid)
+        return bottle.template('views/stran_mapa',
+        grid = grid,
+        )
     else:
-        map = Map(str(map_seed))
+        map = model.Map(str(map_seed))
         map.exception_library()
         map.grid_matrix()
-        return bottle.template('views/stran_mapa', map_seed = map.seed, grid = map.matrix)
+        return bottle.template('views/stran_mapa',
+        map_seed = map.seed, grid = map.matrix,
+        )
+
 
 @bottle.post('/display-map/')
 def display_requested():
@@ -43,6 +72,11 @@ def display_random():
     seed = int(model.generate())
     url = url_mape(seed)
     bottle.redirect(url)
+
+@bottle.route('/roll-dice/', method='POST')
+def roll_dice():
+    dice = bottle.request.forms.getunicode("Die")
+    bottle.redirect('/')
 
 @bottle.error(404)
 def error404(error):
